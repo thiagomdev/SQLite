62aa2c97efa0dbeeb36a6932164ae3f8cd108e10
Thiago Monteiro <thiagomonteiro.developer@gmail.com>
2025-12-10 21:49:16 -0300

    Initial commit

---
+++ app/src/main/java/com/example/sqlite/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.sqlite
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun btnIncluirOnClick(view: View) {}
+    fun btnAlterarOnClick(view: View) {}
+    fun btnExcluirOnClick(view: View) {}
+    fun btnPesquisarOnClick(view: View) {}
+    fun btnListarOnClick(view: View) {}
+}
